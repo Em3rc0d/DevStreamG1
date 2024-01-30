2af0cea5710032d377f43b49dffb1376f16420cc
Ernesto <ernesto.narvarte.c@uni.pe>
2024-01-30 02:36:58 -0500

    homebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from '../login/login.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent],
+  imports: [CommonModule, RouterOutlet, LoginComponent, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'DevStream';
---
+++ src/app/lecciones/lecciones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lecciones',
+  standalone: true,
+  imports: [],
+  templateUrl: './lecciones.component.html',
+  styleUrl: './lecciones.component.css'
+})
+export class LeccionesComponent {
+
+}
