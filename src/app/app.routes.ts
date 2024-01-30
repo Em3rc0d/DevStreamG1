e2cf5b087939d5b7725c9036f93aa65fdfe04053
Ernesto <ernesto.narvarte.c@uni.pe>
2024-01-30 12:03:35 -0500

    no se bajen el proyecto porfa :c

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { LeccionesComponent } from './lecciones/lecciones.component';
+import { LoginComponent } from './login/login.component';
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'menu',
+        component: MenuComponent
+    },
+    {
+        path: 'lecciones',
+        component: LeccionesComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    }
+];
