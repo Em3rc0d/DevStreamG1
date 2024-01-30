5dc572eb1949a9372e196d50f4a57daac7b7649b
Eduardo <farid.merino1673@gmail.com>
2024-01-30 10:30:36 -0500

    x

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
-
+  Mostrar: boolean = false;
 }
