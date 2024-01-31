02956088db5ecc5fef6bf78333319cabb8d823ec
Eduardo <farid.merino1673@gmail.com>
2024-01-31 18:50:16 -0500

    Ready

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { LeccionesComponent } from './lecciones/lecciones.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register/register.component';
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' }, 
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, 
   {
     path: 'login',
     component: LoginComponent,
